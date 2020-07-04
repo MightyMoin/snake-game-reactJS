a3a91eeb3e3b28d8aaacca4acdb9575e14808874
MightyMoin <lcs2019006@iiitl.ac.in>
2020-07-04 22:48:25 +0530

    Updates

---
+++ precache-manifest.3273916483ce9dac0607ea89d699e869.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a27429f9dded7e0c81c2453860231b",
+    "url": "/snake-game-reactJS/index.html"
+  },
+  {
+    "revision": "4b8f53448245ea25897e",
+    "url": "/snake-game-reactJS/static/css/main.b4e4338e.chunk.css"
+  },
+  {
+    "revision": "b99812fdcb999c7aa6dd",
+    "url": "/snake-game-reactJS/static/js/2.015b8eda.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/snake-game-reactJS/static/js/2.015b8eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8f53448245ea25897e",
+    "url": "/snake-game-reactJS/static/js/main.ad3c395a.chunk.js"
+  },
+  {
+    "revision": "7a611b32e230d3aa4abe",
+    "url": "/snake-game-reactJS/static/js/runtime-main.e6c9f95c.js"
+  },
+  {
+    "revision": "f25947491f1eceb3adb9470a174a724e",
+    "url": "/snake-game-reactJS/static/media/dead.f2594749.mp3"
+  },
+  {
+    "revision": "3da54fb71a8e68d6400d1c244ee10abc",
+    "url": "/snake-game-reactJS/static/media/down.3da54fb7.mp3"
+  },
+  {
+    "revision": "ead74d9290f79ff5cb181fefcdf173ed",
+    "url": "/snake-game-reactJS/static/media/eat.ead74d92.mp3"
+  },
+  {
+    "revision": "ebfaf8caad68aea0089431e507d9ceb5",
+    "url": "/snake-game-reactJS/static/media/food.ebfaf8ca.png"
+  },
+  {
+    "revision": "103334eb36a93ccd71c7d8a1fbb1ab7b",
+    "url": "/snake-game-reactJS/static/media/ground.103334eb.png"
+  },
+  {
+    "revision": "efd1952f0cb5e8d25207c6861fb89224",
+    "url": "/snake-game-reactJS/static/media/left.efd1952f.mp3"
+  },
+  {
+    "revision": "de8d6dfbc04607e8a93af1738a0d268f",
+    "url": "/snake-game-reactJS/static/media/right.de8d6dfb.mp3"
+  },
+  {
+    "revision": "58c06899d393ccba69dfd6be08ebff83",
+    "url": "/snake-game-reactJS/static/media/up.58c06899.mp3"
+  }
+]);
